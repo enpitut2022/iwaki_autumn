93524a35010278597743af3ff42184555c69eac8
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-11-27 15:08:17 +0900

    データベース作成プログラムを作成

--- iwaki_autumn/yarukikkake/models.py
+++ iwaki_autumn/yarukikkake/models.py
@@ -1,3 +1,21 @@
+from concurrent.futures import thread
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+class Subject(models.Model):
+    code = models.CharField(verbose_name="科目番号", max_length=7, primary_key=True)
+    name = models.CharField(verbose_name='科目名',blank=False, null=False, max_length=150)
+    unit = models.CharField(verbose_name="単位数",blank=False, null=False, default="", max_length=10)
+    grade = models.CharField(verbose_name="履修年次",blank=False, null=False, default="", max_length=30)
+    semester = models.CharField(verbose_name="学期",blank=False, null=False, default="", max_length=30)
+    teachers =  models.TextField(verbose_name='教員名', blank=True, max_length=100, default="")
+    overview = models.TextField(verbose_name='概要', blank=True, max_length=200, default="")
+    subtype =  models.TextField(verbose_name='種類', blank=True, max_length=32, default="")
+    schools = models.CharField(verbose_name="学群等", blank=True,max_length=40, default="")
+    colleges = models.CharField(verbose_name="学類等", blank=True, max_length=40, default="")
+
+    def __str__(self):
+        return self.code + " " + self.name
+
+
--- iwaki_autumn/yarukikkake/views.py
+++ iwaki_autumn/yarukikkake/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-from utils import message_creater
-from line_bot.line_message import LineMessage
+from .message_create import create_single_text_message
+from .line_message import LineMessage
 
 @csrf_exempt
 def index(request):
@@ -13,6 +13,8 @@ def index(request):
         data = request['events'][0]
         message = data['message']
         reply_token = data['replyToken']
-        line_message = LineMessage(message_creater.create_single_text_message(message['text']))
+        line_message = LineMessage(create_single_text_message(message['text']))
         line_message.reply(reply_token)
         return HttpResponse("ok")
+    else:
+        return HttpResponse("やるきっかけ LINE Bot")
