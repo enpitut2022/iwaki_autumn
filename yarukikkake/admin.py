39d7d13b76147995456be49a677939225cc1c408
amixedcolor <amixedcolor@gmail.com>
2022-11-29 15:58:12 +0900

    admin画面でdbの管理ができる

--- yarukikkake/admin.py
+++ yarukikkake/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Subject
+
 # Register your models here.
+
+class SubjectAdmin(admin.ModelAdmin):
+    fields = ["code", "name", "unit", "grade", "semester", "teachers", "subtype", "schools", "colleges"]
+    
+admin.site.register(Subject, SubjectAdmin)
