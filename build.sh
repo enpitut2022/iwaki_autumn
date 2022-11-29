3f2e2bb41a0a412af52a82d521c9deb0c7c0615a
amixedcolor <amixedcolor@gmail.com>
2022-11-29 15:07:19 +0900

    yamlの中身から'test'と余計についていたのを削除

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py create_database
 python manage.py create_superuser
