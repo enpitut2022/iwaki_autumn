ff00b0e10247f475189552ea60660ee033845261
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-12-14 13:07:48 +0900

    勉強会一覧を出す

--- yarukikkake/message_create.py
+++ yarukikkake/message_create.py
@@ -1,6 +1,8 @@
 def create_single_text_message(message):
     if message == '予定':
-        message = 'やるきかっけの予定が生成されました！\n 題名：レスポンもくもく会\n 11/16 15時よりこちらのURLから参加できます！\n https://discord.com/channels/963633143317938176/987247856991744020'
+        message = 'やるきっかけの予定が生成されました！\n 題名：レスポンもくもく会\n 11/16 15時よりこちらのURLから参加できます！\n https://discord.com/channels/963633143317938176/987247856991744020'
+    elif message == '今日の勉強会':
+        message = '並列処理アーキテクチャの第9回\n情報線形代数第9回\n専門英語基礎第9回\nなどがあります！\n'
     test_message = [
                 {
                     'type': 'text',
