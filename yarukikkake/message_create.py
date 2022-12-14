2c82db7fab0dee5d0202510d51d7e675bf383773
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-12-14 13:26:42 +0900

    データベースから授業情報を引っ張ってこれるようになった。

--- yarukikkake/message_create.py
+++ yarukikkake/message_create.py
@@ -1,8 +1,13 @@
+from .models import Subject
+
 def create_single_text_message(message):
     if message == '予定':
         message = 'やるきっかけの予定が生成されました！\n 題名：レスポンもくもく会\n 11/16 15時よりこちらのURLから参加できます！\n https://discord.com/channels/963633143317938176/987247856991744020'
     elif message == '今日の勉強会':
-        message = '並列処理アーキテクチャの第9回\n情報線形代数第9回\n専門英語基礎第9回\nなどがあります！\n'
+        lectures = Subject.objects.filter(semester="秋AB", schools="情報学群")
+        for lecture in lectures:
+            message += (lecture.name + '第9回\n')
+        message += 'などがあります！'
     test_message = [
                 {
                     'type': 'text',
