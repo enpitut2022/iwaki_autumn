c2befed8f762656fcca8161954ea464fa06c1b48
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-12-14 13:58:09 +0900

    勉強会検索ができるようになった

--- yarukikkake/message_create.py
+++ yarukikkake/message_create.py
@@ -8,6 +8,14 @@ def create_single_text_message(message):
         for lecture in lectures:
             message += (lecture.name + '第9回\n')
         message += 'などがあります！'
+    elif message == 'ヘルプ':
+        message = 'やるきっかけの使い方\n\n予定\n今日の勉強会\n科目番号\n\nの3つのコマンドがあります。'
+    else:
+        lectures = Subject.objects.filter(semester="秋AB", schools="情報学群", code=message)
+        if lectures != None :
+            message =  "【題名】:"+lectures.name+"#9をやっつける会\n【対象学類】:情報科学類生(他学類も可)\n【日時】:12/12 20時から1時間程度(途中入退出OK)\n好きなところのURLから参加しよう!\n\n【Discord(音声)】https://discord.com/channels/963633143317938176/997416162218483782 \n"
+        else :
+            message = "この授業の勉強会の予定はありません"
     test_message = [
                 {
                     'type': 'text',
